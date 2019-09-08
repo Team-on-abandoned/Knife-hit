a1152c0cdf4dc6da2f6be6e2e264dcfd06916965
Team-on <timonsol13@gmail.com>
2019-09-08 03:58:24 +0300

    Target hit effects

---
+++ KnifeHit/Assets/Scripts/Target/TargetBlink.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetBlink : MonoBehaviour {
+	[SerializeField] SpriteRenderer SpriteRenderer;
+
+	void Awake() {
+		EventManager.OnTargetHit += OnTargetHit;
+	}
+
+	void OnDestroy() {
+		LeanTween.cancel(gameObject, false);
+		EventManager.OnTargetHit -= OnTargetHit;
+	}
+
+	void OnTargetHit(EventData eventData) {
+		LeanTween.value(gameObject, 0.0f, 0.25f, 0.05f)
+			.setOnUpdate((float val) => {
+				SpriteRenderer.material.SetFloat("_FlashAmount", val);
+			})
+			.setOnComplete(()=> {
+				LeanTween.value(gameObject, 0.25f, 0.0f, 0.05f)
+				.setOnUpdate((float val) => {
+					SpriteRenderer.material.SetFloat("_FlashAmount", val);
+				});
+			});
+	}
+}
---
+++ KnifeHit/Assets/Scripts/Target/TargetShake.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetShake : MonoBehaviour {
+	public float Speed = 0.0001f;
+
+	Vector2 startPos;
+	Coroutine shakeCoroutine;
+
+	void Awake() {
+		startPos = transform.position;
+
+		EventManager.OnTargetHit += OnTargetHit;
+	}
+
+	void OnDestroy() {
+		if(shakeCoroutine != null)
+			StopCoroutine(shakeCoroutine);
+		EventManager.OnTargetHit -= OnTargetHit;
+	}
+
+	void OnTargetHit(EventData eventData) {
+		shakeCoroutine = StartCoroutine(ShakeCoroutine());
+	}
+
+	IEnumerator ShakeCoroutine() {
+		for(byte i = 0; i < 5; ++i) {
+			gameObject.transform.position = startPos + Random.insideUnitCircle * Speed * Time.deltaTime;
+			yield return null;
+		}
+		gameObject.transform.position = startPos;
+	}
+}
