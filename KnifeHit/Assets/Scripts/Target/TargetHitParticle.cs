2c4925718c1c4080bf3edb935d2d00609751214e
Team-on <timonsol13@gmail.com>
2019-09-09 01:40:37 +0300

    Particles on hit

---
+++ KnifeHit/Assets/Scripts/Target/TargetHitParticle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetHitParticle : MonoBehaviour {
+	[SerializeField] GameObject ParticlePrefab;
+
+	void Awake() {
+		EventManager.OnTargetHit += OnTargetHit;
+	}
+
+	void OnDestroy() {
+		EventManager.OnTargetHit -= OnTargetHit;
+	}
+
+	void OnTargetHit(EventData eventData) {
+		ParticleSystem particle = Instantiate(ParticlePrefab, new Vector3(0, 0.86f, -1), Quaternion.identity, transform).GetComponent<ParticleSystem>();
+		particle.Play();
+	}
+}
---
+++ KnifeHit/Assets/Scripts/UI/ParticleDestroyer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class ParticleDestroyer : MonoBehaviour {
+	public ParticleSystem ParticleSystem;
+
+	void Update() {
+		if (!ParticleSystem.IsAlive())
+			Destroy(gameObject);
+	}
+}
