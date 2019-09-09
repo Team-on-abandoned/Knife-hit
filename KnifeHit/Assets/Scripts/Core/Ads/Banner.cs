e6d164f181c545e09cc8ffd251954aa45791df08
Team-on <timonsol13@gmail.com>
2019-09-09 03:21:38 +0300

    Add banner

---
+++ KnifeHit/Assets/Scripts/Core/Ads/Banner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+using UnityEngine.Monetization;
+
+public class Banner : MonoBehaviour {
+	public string BannerId;
+	public BannerPosition BannerPosition;
+
+	void Start() {
+		StartCoroutine(ShowBannerWhenReady());
+	}
+
+	IEnumerator ShowBannerWhenReady() {
+		while (!Advertisement.IsReady(BannerId))
+			yield return new WaitForSeconds(0.5f);
+
+		Advertisement.Banner.Show(BannerId);
+		Advertisement.Banner.SetPosition(BannerPosition);
+
+		yield return null;
+
+		Destroy(gameObject);
+	}
+}
